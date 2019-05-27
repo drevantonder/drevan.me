5ff9223b5a481b06d646e998771be4af4e1ac97d
Andre van Tonder <drevtonder@gmail.com>
2019-05-27 20:50:41 +1000

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
     name: 'drevantonder.io',
     script: 'server/index.js',
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
     autorestart: true,
     watch: true,
