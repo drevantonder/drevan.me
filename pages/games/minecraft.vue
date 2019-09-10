af8842e7385e58089699fda8e3cd9ddfbbe5f096
Andre van Tonder <drevtonder@gmail.com>
2019-09-10 17:15:15 +1000

    Add minecraft page

---
+++ pages/games/minecraft.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <article>
+      <h1>Minecraft</h1>
+      <h2>Rules</h2>
+      <p>
+        1. Everyone is free to play how they want, but people in creative should build a distance from where survival buildings are, and creative players should NOT give people items
+        2. For ballopower, no one is to tp to ballopower without asking first, and you MUST be in survival while you are there. If he says no, you must respect that, if you tp OP privileges will be taken from you
+        3. No more griefing. We are not children, we can behave like adults (Well, I hope so)
+        4. No swearing ever...
+      </p>
+    </article>
+  </main>
+</template>
