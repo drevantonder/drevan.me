82e533db46577f33a9ee63ab10fc2592f83a5068
Andre van Tonder <drevtonder@gmail.com>
2020-04-24 16:14:06 +1000

    Add margin for links

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -18,7 +18,7 @@
           v-editable="link"
           :href="link.link.url"
           class="text-blue-600 bg-gray-100 px-6 py-3 rounded-lg text-lg font-medium"
-        ><fa-icon :icon="[link.icon[0].type, link.icon[0].icon]" />{{ link.text }}</a>
+        ><fa-icon class="mr-2" :icon="[link.icon[0].type, link.icon[0].icon]" />{{ link.text }}</a>
       </div>
     </div>
   </div>
