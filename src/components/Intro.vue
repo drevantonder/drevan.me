c363f1b50af5c4f1010b0d2716b8d923485978dc
Andre van Tonder <drevtonder@gmail.com>
2020-04-24 15:50:17 +1000

    Go back to original design

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,29 +1,25 @@
 <template>
   <div class="min-h-screen flex flex-col justify-center">
-    <div class="sm:mx-auto sm:w-full sm:max-w-2xl flex">
-      <div>
-        <g-image 
-          :src="imageURL" 
-          width="64" 
-          class="rounded-full shadow-xl mx-auto"
-        />
-      </div>
-      <div>
-        <h1 class="text-5xl text-gray-100 text-center font-medium tracking-tight leading-tight">
-          {{ blok.name }}
-        </h1>
-        <h2 class="text-2xl text-gray-200 text-center leading-tight">
-          {{ blok.text }}
-        </h2>
-        <div class="flex justify-around mt-6">
-          <a
-            v-for="link in blok.links"
-            :key="link.uuid"
-            v-editable="link"
-            :href="link.link.url"
-            class="text-blue-600 bg-gray-100 px-6 py-3 rounded-lg text-lg font-medium"
-          >{{ link.text }}</a>
-        </div>
+    <div class="sm:mx-auto sm:w-full sm:max-w-2xl">
+      <g-image 
+        :src="imageURL" 
+        width="64" 
+        class="rounded-full shadow-xl mx-auto"
+      />
+      <h1 class="text-5xl text-gray-100 text-center font-medium tracking-tight leading-tight">
+        {{ blok.name }}
+      </h1>
+      <h2 class="text-2xl text-gray-200 text-center leading-tight">
+        {{ blok.text }}
+      </h2>
+      <div class="flex justify-around mt-6">
+        <a
+          v-for="link in blok.links"
+          :key="link.uuid"
+          v-editable="link"
+          :href="link.link.url"
+          class="text-blue-600 bg-gray-100 px-6 py-3 rounded-lg text-lg font-medium"
+        >{{ link.text }}</a>
       </div>
     </div>
   </div>
