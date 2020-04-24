f8b50c8a145d0f1005329dd92c058df16dec703a
Andre van Tonder <drevtonder@gmail.com>
2020-04-24 16:13:28 +1000

    Add icons

--- src/main.js
+++ src/main.js
@@ -5,9 +5,17 @@ import DefaultLayout from '~/layouts/Default.vue'
 import Page from '~/components/Page.vue'
 import Intro from '~/components/Intro.vue'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTwitter, faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTwitter, faGithub, faStackOverflow)
+
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
   Vue.component('Page', Page)
   Vue.component('Intro', Intro)
+
+  Vue.component('FaIcon', FontAwesomeIcon)
 }
