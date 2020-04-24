e68f1736c007d2568aa15a2aa954de928816d753
Andre van Tonder <drevtonder@gmail.com>
2020-04-24 15:44:21 +1000

    Try fix build

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="min-h-screen flex flex-col justify-center">
-    <div class="sm:mx-auto sm:w-full sm:max-w-xl">
-      <g-image 
-        :src="blok.image" 
-        width="100" 
-        class="rounded-full shadow-xl mx-auto"
-      />
-      <h1 class="text-5xl text-white text-center font-medium tracking-tight">
-        {{ blok.name }}
-      </h1>
-      <h2 class="text-3xl text-white text-center">
-        {{ blok.text }}
-      </h2>
-      <div class="flex justify-around mt-3">
-        <a
-          v-for="link in blok.links"
-          :key="link.uuid"
-          v-editable="link"
-          :href="link.link.url"
-          class="text-blue-600 bg-white px-6 py-3 rounded-lg text-lg font-medium"
-        >{{ link.text }}</a>
+    <div class="sm:mx-auto sm:w-full sm:max-w-2xl flex">
+      <div>
+        <g-image 
+          :src="blok.image" 
+          width="64" 
+          class="rounded-full shadow-xl mx-auto"
+        />
+      </div>
+      <div>
+        <h1 class="text-5xl text-gray-100 text-center font-medium tracking-tight leading-tight">
+          {{ blok.name }}
+        </h1>
+        <h2 class="text-2xl text-gray-200 text-center leading-tight">
+          {{ blok.text }}
+        </h2>
+        <div class="flex justify-around mt-6">
+          <a
+            v-for="link in blok.links"
+            :key="link.uuid"
+            v-editable="link"
+            :href="link.link.url"
+            class="text-blue-600 bg-gray-100 px-6 py-3 rounded-lg text-lg font-medium"
+          >{{ link.text }}</a>
+        </div>
       </div>
     </div>
   </div>
@@ -31,6 +35,19 @@ export default {
     blok: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    imageURL () {
+      // When options.downloadImages is false, the image property is a string
+      if (typeof this.blok.image === 'string') {
+        return this.blok.image
+      }
+
+      // When options.downloadImages is true, the image property is a object
+      // Reference of this: https://github.com/gridsome/gridsome/issues/292
+      const path = this.blok.image.path
+      return require('!!assets-loader?width=64&quality=100&fit=inside!~/' + path)
     }
   }
 }
