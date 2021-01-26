50fd9cc7d3b4412f2e948cd830d1f540fe441a7a
Andre van Tonder <drevan@hey.com>
2021-01-26 13:17:21 +1000

    Start adding articles

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="max-w-3xl">
-    <article class="prose prose-2xl">
-      <h1>Hello 👋</h1>
-      <h2>My name is Andre van Tonder.</h2>
-      <p>
-        I am a Christian ⛪, Software Developer 💻 and Star Wars fan 🌌. I love
-        learning new things and meeting new people.
-      </p>
-      <p>
-        I currently work full time at Creation Ministries (Australia). I enjoy
-        coding on personal projects and freelancing in my spare time.
-      </p>
-      <p>While you are here, feel free to check out some of my articles ➡.</p>
-    </article>
+  <div class="flex max-w-screen-2xl mx-auto">
+    <div class="flex min-h-screen items-center flex-shrink-0">
+      <article class="prose prose-2xl">
+        <h1>Hello 👋</h1>
+        <h2>My name is Andre van Tonder.</h2>
+        <p>
+          I am a Christian ⛪, Software Developer 💻 and Star Wars fan 🌌. I
+          love learning new things and meeting new people.
+        </p>
+        <p>
+          I currently work full time at Creation Ministries (Australia). I enjoy
+          coding on personal projects and freelancing in my spare time.
+        </p>
+        <p>While you are here, feel free to check out some of my articles ➡.</p>
+      </article>
+    </div>
+    <aside class="flex flex-col items-center w-full mt-24">
+      <article class="shadow-xl rounded-lg max-w-sm w-full p-8 bg-white">
+        <time datetime="2021-01-26" class="uppercase font-medium text-gray-500">
+          26 Dec, 2021
+        </time>
+        <h3 class="text-3xl font-semibold text-gray-700">My Awesome List</h3>
+      </article>
+    </aside>
   </div>
 </template>
 
