81cd74582e4470430d06a28baaa9e0bc7f30bfd7
Andre van Tonder <drevan@hey.com>
2021-02-14 17:27:49 +1000

    Improve mobile experience

---
+++ components/article/ArticleGrid.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="grid grid-cols-1 sm:grid-cols-2 gap-8">
+    <article-preview
+      v-for="article in articles"
+      :key="article._id"
+      :article="article"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    articles: {
+      type: Array,
+      required: true,
+    },
+  },
+})
+</script>
--- components/page/PageNav.vue
+++ components/page/PageNav.vue
@@ -1,14 +1,24 @@
 <template>
-  <nav class="fixed left-0 inset-y-0 py-20 pl-4">
-    <ul class="flex flex-col space-y-6">
+  <nav
+    class="fixed bottom-0 lg:inset-y-0 inset-x-0 lg:right-auto lg:left-0 bg-teal-50 lg:bg-transparent bg-opacity-80 lg:bg-opacity-100 py-1 lg:py-20 lg:pl-4 z-10 lg:z-0 shadow-xl lg:shadow-none"
+  >
+    <ul
+      class="flex flex-row lg:flex-col justify-around lg:justify-start space-x-6 lg:space-x-0 lg:space-y-6"
+    >
       <li>
-        <nuxt-link to="/" class="text-gray-700 font-medium text-xl">
-          <span class="text-2xl">🏡</span>&nbsp;Home
+        <nuxt-link
+          to="/"
+          class="text-gray-700 font-medium text-base lg:text-xl flex flex-col lg:flex-row items-center lg:items-start"
+        >
+          <span class="text-xl lg:text-2xl">🏡</span>&nbsp;Home
         </nuxt-link>
       </li>
       <li>
-        <nuxt-link to="/articles" class="text-gray-700 font-medium text-xl">
-          <span class="text-2xl">📚</span>&nbsp;Articles
+        <nuxt-link
+          to="/articles"
+          class="text-gray-700 font-medium text-base lg:text-xl flex flex-col lg:flex-row items-center lg:items-start"
+        >
+          <span class="text-xl lg:text-2xl">📚</span>&nbsp;Articles
         </nuxt-link>
       </li>
     </ul>
