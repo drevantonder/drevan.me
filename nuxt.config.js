73cbfc99af20aa0bca1c81ebd45346ebfbf7b2e2
Andre van Tonder <drevan@hey.com>
2021-03-06 12:21:49 +1000

    Update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'drevantonder.io',
+    title: 'Andre van Tonder',
     htmlAttrs: {
       lang: 'en',
     },
