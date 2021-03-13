cbb4b649b7c9f5d8cf6f50a3870ca42cb5b1b8e7
Andre van Tonder <drevan@hey.com>
2021-03-13 16:39:44 +1000

    Update PageNav

--- components/page/nav/PageNav.vue
+++ components/page/nav/PageNav.vue
@@ -1,27 +1,30 @@
 <template>
-  <nav
-    class="fixed bottom-0 lg:inset-y-0 inset-x-0 lg:right-auto lg:left-0 bg-teal-50 lg:bg-transparent bg-opacity-80 lg:bg-opacity-100 py-1 lg:py-20 lg:pl-4 z-10 lg:z-0 shadow-xl lg:shadow-none"
-  >
-    <ul
-      class="flex flex-row lg:flex-col justify-around lg:justify-start space-x-6 lg:space-x-0 lg:space-y-6"
-    >
+  <nav class="fixed inset-y-0 right-auto left-0 py-20 pl-4">
+    <ul class="space-y-6">
       <li>
         <nuxt-link
           to="/"
-          class="text-gray-700 dark:text-gray-400 font-medium text-base lg:text-xl flex flex-col lg:flex-row items-center lg:items-start"
-        >
-          <span class="text-xl lg:text-2xl">🏡</span>&nbsp;Home
+          class="text-gray-700 dark:text-gray-400 font-medium text-xl"
+          >Home
         </nuxt-link>
       </li>
       <li>
         <nuxt-link
           to="/articles"
-          class="text-gray-700 dark:text-gray-400 font-medium text-base lg:text-xl flex flex-col lg:flex-row items-center lg:items-start"
-        >
-          <span class="text-xl lg:text-2xl">📚</span>&nbsp;Articles
+          class="text-gray-700 dark:text-gray-400 font-medium text-xl"
+          >Articles
         </nuxt-link>
       </li>
     </ul>
     <page-nav-color-scheme class="mt-6" />
   </nav>
 </template>
+
+<style lang="postcss" scoped>
+li {
+  &::before {
+    content: '➜';
+    @apply text-teal-400 font-medium text-xl mr-2;
+  }
+}
+</style>
