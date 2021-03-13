01900e984d08c516a6c11633bcf9d35020828d3f
Andre van Tonder <drevan@hey.com>
2021-03-13 12:13:43 +1000

    Remove @tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,39 +14,6 @@ module.exports = {
       current: 'currentColor',
     },
     extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            'a:not(.heading-link)': {
-              whiteSpace: 'nowrap',
-              textDecoration: 'none',
-              position: 'relative',
-              color: 'transparent',
-              background: `linear-gradient(to right, ${colors.lightBlue[500]} 0%, ${colors.teal[500]} 100%)`,
-              backgroundClip: 'text',
-              transition: 'color .2s',
-              '&:hover': {
-                color: colors.white,
-              },
-            },
-            'a:not(.heading-link)::after': {
-              content: "''",
-              position: 'absolute',
-              left: '-0.05em',
-              right: '-0.05em',
-              bottom: '0',
-              top: '95%',
-              background: `linear-gradient(to right, ${colors.lightBlue[500]} 0%, ${colors.teal[500]} 100%)`,
-              borderRadius: '4px',
-              transition: 'top .2s',
-              'z-index': -1,
-            },
-            'a:not(.heading-link):hover::after': {
-              top: '0',
-            },
-          },
-        },
-      },
       zIndex: {
         '-1': '-1',
       },
@@ -58,5 +25,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [],
 }
