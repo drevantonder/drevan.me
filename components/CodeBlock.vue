3c603321c23e8a34cbc0c48d67978aaf775f5afb
Andre van Tonder <drevan@hey.com>
2021-04-24 20:42:45 +1000

    Add CodeBlock and CodeGroup

---
+++ components/CodeBlock.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-show="active">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tab: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    active() {
+      // Using $parent is not recommended, however slotsProps don't work in @nuxt/content
+      // SSee: https://content.nuxtjs.org/writing#templates
+      return this.$parent.activeTab === this.tab
+    },
+  },
+}
+</script>
---
+++ components/CodeGroup.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :class="`code-group ${activeTab === tabs[0] && 'first-tab'}`">
+    <div class="-mb-2 flex space-x-1">
+      <button
+        v-for="tab in tabs"
+        :key="tab"
+        :class="`rounded-t-lg text-lg font-semibold px-3 py-1 
+          focus:outline-none focus:ring-2 focus:ring-teal-500
+        ${activeTab === tab ? 'bg-gray-800 text-teal-500' : 'text-gray-400'}`"
+        @click="activeTab = tab"
+      >
+        {{ tab }}
+      </button>
+    </div>
+    <slot :activeTab="activeTab" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    startTab: {
+      type: String,
+      default: null,
+    },
+
+    tabs: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      activeTab: this.startTab || this.tabs[0],
+    }
+  },
+}
+</script>
+
+<style lang="postcss">
+.code-group.first-tab {
+  pre[class*='language-'] {
+    @apply rounded-t-none;
+  }
+}
+</style>
