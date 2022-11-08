421ed5c011a467b9ec63410f20bf684ca3e2ba43
Andre van Tonder <drevan@hey.com>
2022-11-08 22:47:18 +1000

    feat: Add `@astrojs/sitemap`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import alpinejs from '@astrojs/alpinejs';
 
-import alpinejs from '@astrojs/alpinejs'
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()],
+  integrations: [tailwind(), alpinejs(), sitemap()],
   site: 'https://drevan.me/',
   trailingSlash: 'always'
-})
+});
