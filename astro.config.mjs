703ef5d055041e407b90b179dc4ee82ff626816b
Andre van Tonder <drevan@hey.com>
2022-11-08 23:17:12 +1000

    feat: Add `astro-robots-txt` to add `robots.txt`

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import alpinejs from '@astrojs/alpinejs';
 
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), sitemap()],
+  integrations: [tailwind(), alpinejs(), sitemap(), robotsTxt()],
   site: 'https://drevan.me/',
   trailingSlash: 'always'
 });
