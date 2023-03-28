71dd203f5053b6e43a0e4451f8a23e5bd4e8375f
Andre van Tonder <andre@drevan.me>
2023-03-28 23:15:48 +1000

    Add MDX

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+const withMDX = require('@next/mdx')({
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   experimental: {
     appDir: true,
   },
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
