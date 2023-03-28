3f747df0779e2930834e1ecea48e8254fe4cdec4
Andre van Tonder <andre@drevan.me>
2023-03-28 22:57:02 +1000

    Add content paths for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
