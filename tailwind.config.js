776170e683173651d797f7270fa8f57e526bd7b0
Andre van Tonder <andre@drevan.me>
2023-03-30 00:16:07 +1000

    Add social links

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -7,6 +8,27 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      cyan: {
+        400: '#69E1E9',
+      },
+      purple: {
+        50: '#F5F6FF',
+        100: '#CACFFF',
+        200: '#A1A5FF',
+        300: '#7D7EFE',
+        400: '#6460FD',
+        500: '#524AFA',
+        600: '#4539F4',
+        700: '#392CE5',
+        800: '#2C1FBE',
+        900: '#1A1278',
+      },
+      gray: colors.zinc,
+    },
     extend: {
       fontFamily: {
         sans: ['var(--custom-font)', ...defaultTheme.fontFamily.sans],
