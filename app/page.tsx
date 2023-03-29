22aec19720bc78b2cbc92c4571adeeb0a333e07e
Andre van Tonder <andre@drevan.me>
2023-03-30 00:03:06 +1000

    Add simple layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import { Container } from '@/components/Container'
+import Link from 'next/link'
 import Intro from './intro.mdx'
 
 export default function Home() {
   return (
-    <Container>
-      <div className="prose prose-lg lg:prose-xl">
-        <Intro />
-      </div>
-    </Container>
+      <Container>
+        <nav className="mt-8">
+          <ul className="flex space-x-8">
+            <li><Link href="/photos/">@photosbydrevan</Link></li>
+            <li><Link href="/blog">blog</Link></li>
+          </ul>
+        </nav>
+        <div className="mt-32 prose prose-lg lg:prose-xl">
+          <Intro />
+        </div>
+      </Container>
   )
 }
