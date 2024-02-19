6d14bd64736a42ba342d4cc2b7f3ab7990639693
Andre van Tonder <andre@drevan.me>
2024-02-19 21:49:21 +1000

    Some basics

---
+++ components/ColourMode.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <select v-model="$colorMode.preference">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+    </select>
+  </div>
+</template>
+
+<script setup>
+const colorMode = useColorMode();
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="container mx-auto mt-32">
+        <h1 class="text-5xl font-serif text-center">Slavery in the Bible?</h1>
+        <p class="text-center mt-4">Lorem ipsum doner se amet</p>
+    </div>
+</template>
