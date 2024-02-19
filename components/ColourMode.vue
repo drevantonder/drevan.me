d9ccf518222f41a6c56367d20ed53c7ace8b2dfc
Andre van Tonder <andre@drevan.me>
2024-02-19 22:26:41 +1000

    Add hover effects

--- components/ColourMode.vue
+++ components/ColourMode.vue
@@ -3,7 +3,7 @@ const colorMode = useColorMode();
 </script>
 
 <template>
-  <button type="button" @click="colorMode.value = colorMode.value === 'dark' ? 'light' : 'dark'">
+  <button type="button" @click="colorMode.value = colorMode.value === 'dark' ? 'light' : 'dark'" class="p-2 rounded-md hover:bg-[#f5e8e5] dark:hover:bg-neutral-900">
     <Icon :name="colorMode.value === 'dark' ? 'ph:sun-duotone' : 'ph:moon-duotone'" class="text-neutral-900 dark:text-neutral-50 size-5" />
   </button>
 </template>
