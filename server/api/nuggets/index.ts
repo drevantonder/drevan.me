1a1613b20bc6c807e100c581f87535a6fc6575f2
Andre van Tonder <andre@drevan.me>
2024-02-27 23:02:08 +1000

    Add `readItems` with filter

--- server/api/nuggets/index.ts
+++ server/api/nuggets/index.ts
@@ -1,3 +1,13 @@
 export default defineEventHandler(async (event) => {
-  return Nuggets.readAllItems();
+  const { limit } = getQuery(event);
+  let page_size: number | undefined = undefined;
+  if (typeof limit === "number") {
+    page_size = limit;
+  } else if (typeof limit === "string") {
+    page_size = parseInt(limit);
+  }
+
+  return Nuggets.readItems({
+    limit: page_size,
+  });
 });
