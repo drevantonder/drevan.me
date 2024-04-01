cf3ef2396054289d826e5a607d38561149c5c37d
Andre van Tonder <andre@drevan.me>
2024-04-01 20:01:07 +1000

    Pull content from sanity

--- apps/drevan.me/server/utils/useNotion.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "@notionhq/client";
-
-export const useNotion = () => {
-  const runtimeConfig = useRuntimeConfig();
-
-  return new Client({
-    auth: runtimeConfig.notionToken,
-  });
-};
