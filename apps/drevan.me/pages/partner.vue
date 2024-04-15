177ff8677278f1cd755336a5a5e9a7563e78a05b
Andre van Tonder <a.vantonder@creation.com>
2024-04-15 18:21:24 +1000

    Rough blog post and partners page

---
+++ apps/drevan.me/pages/partner.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Container>
+    <h1
+      class="font-serif text-lg tracking-wide dark:text-neutral-200 text-neutral-600 mt-6"
+    >
+      Partner with me
+    </h1>
+  </Container>
+</template>
