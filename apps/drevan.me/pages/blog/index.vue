55893d77ce4a8f1c21303c83abcc7addf12d7e14
Andre van Tonder <a.vantonder@creation.com>
2024-04-15 16:57:22 +1000

    Start adding the blog

---
+++ apps/drevan.me/pages/blog/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Container>
+    <h1
+      class="font-serif text-lg tracking-wide dark:text-neutral-200 text-neutral-600 mt-6"
+    >
+      André's Blog
+    </h1>
+    <section class="mt-2">
+      <h2 class="text-xs text-semibold uppercase sr-only">Recent</h2>
+      <div class="grid sm:grid-cols-3 gap-y-4 gap-x-12 -mx-4 sm:-mx-8">
+        <a
+          href="/blog/joining-power-to-change"
+          class="w-full sm:max-w-xs bg-neutral-100 dark:bg-neutral-900 px-4 sm:rounded-xl py-3 shadow-lg border border-neutral-200/50 dark:border-neutral-800/50"
+        >
+          <article>
+            <h3>Joining Power to Change</h3>
+          </article>
+        </a>
+      </div>
+    </section>
+  </Container>
+</template>
